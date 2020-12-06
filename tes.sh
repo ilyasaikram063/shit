7a35de80177f34147b452b190e154e8c9e05df80
ilyasaikram063 <74800927+ilyasaikram063@users.noreply.github.com>
2020-12-07 05:33:57 +0700

    Create tes.sh

---
+++ tes.sh
@@ -0,0 +1,49 @@
+fgu="ffmpeg -re -i"
+ 
+mnctv=' "https://vcdn2.rctiplus.id/live/eds/mnctv_fta/live_fta/mnctv_fta-avc1_2000000=1-mp4a_64000_eng=2.m3u8" '
+ 
+rcti=' "https://vcdn2.rctiplus.id/live/eds/rcti_fta/live_fta/rcti_fta-avc1_2000000=1-mp4a_64000_eng=2.m3u8" '
+ 
+gtv=' "https://vcdn2.rctiplus.id/live/eds/gtv_fta/live_fta/gtv_fta-avc1_1000000=3-mp4a_64000_eng=2.m3u8" '
+ 
+cnn=' "https://live.cnnindonesia.com/livecnn/smil:cnntv.smil/playlist.m3u8" '
+ 
+tvone='youtube-dl -f best "https://youtu.be/XdZ2nJvmfrI" -o - | ffmpeg -re -i - -ar 44100 -vcodec copy -f flv "rtmp://localhost:8888/rtmp/'
+ 
+fga="-c:v copy -c:a aac -ar 44100 -ab 128k -ac 2 -strict -2 -flags +global_header -bsf:a aac_adtstoasc -bufsize 3000k -f flv"
+ 
+token=' "rtmp://localhost:8888/rtmp/'
+
+echo '
+DAFTAR CHANNEL
+ 
+1. mnctv
+2. gtv
+3. rcti
+4. cnn
+5. tvone
+'
+
+read -p "pilih nomer channel: " jawab
+read -p "masukan key-live-stream fb: " key
+ 
+if [ $jawab -eq 1 ];
+then
+echo "$fgu$mnctv$fga$token$key"
+elif [ $jawab -eq 2 ];
+then
+echo "$fgu$gtv$fga$token$key"
+elif [ $jawab -eq 3 ];
+then
+echo "$fgu$rcti$fga$token$key"
+elif [ $jawab -eq 4 ];
+then
+echo "$fgu$cnn$fga$token$key"
+elif [ $jawab -eq 5 ];
+then
+echo "$tvone$key"
+else
+echo '
+aduh, aya nu salah yas
+'
+fi
