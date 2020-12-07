bcbb4d42118358b39facd5e77e947a13c18eeae7
ilyasaikram063 <74800927+ilyasaikram063@users.noreply.github.com>
2020-12-08 06:02:20 +0700

    Update can.sh

--- can.sh
+++ can.sh
@@ -72,33 +72,25 @@ sh mnctv.sh
 elif [ $jawab -eq 2 ];
 then
 cd tv
-echo 'ffmpeg -re -i "https://vcdn2.rctiplus.id/live/eds/gtv_fta/live_fta/gtv_fta-avc1_1000000=3-mp4a_64000_eng=2.m3u8" -c:v copy -c:a aac -ar 44100 -ab 128k -ac 2 -strict -2 -flags +global_header -bsf:a aac_adtstoasc -bufsize 3000k -f flv "rtmp://localhost:8888/rtmp/' > gtv.sh
-cd
-cd shit/tv/
+echo 'ffmpeg -re -i "https://vcdn2.rctiplus.id/live/eds/gtv_fta/live_fta/gtv_fta-avc1_1000000=3-mp4a_64000_eng=2.m3u8" -c:v copy -c:a aac -ar 44100 -ab 128k -ac 2 -strict -2 -flags +global_header -bsf:a aac_adtstoasc -bufsize 3000k -f flv "rtmp://localhost:8888/rtmp/"' > gtv.sh
 nano gtv.sh
 sh gtv.sh
 elif [ $jawab -eq 3 ];
 then
 cd tv
-echo 'ffmpeg -re -i "https://vcdn2.rctiplus.id/live/eds/rcti_fta/live_fta/rcti_fta-avc1_2000000=1-mp4a_64000_eng=2.m3u8" -c:v copy -c:a aac -ar 44100 -ab 128k -ac 2 -strict -2 -flags +global_header -bsf:a aac_adtstoasc -bufsize 3000k -f flv "rtmp://localhost:8888/rtmp/' > rcti.sh
-cd
-cd shit/tv/
+echo 'ffmpeg -re -i "https://vcdn2.rctiplus.id/live/eds/rcti_fta/live_fta/rcti_fta-avc1_2000000=1-mp4a_64000_eng=2.m3u8" -c:v copy -c:a aac -ar 44100 -ab 128k -ac 2 -strict -2 -flags +global_header -bsf:a aac_adtstoasc -bufsize 3000k -f flv "rtmp://localhost:8888/rtmp/"' > rcti.sh
 nano rcti.sh
 sh rcti.sh
 elif [ $jawab -eq 4 ];
 then
 cd tv
-echo 'ffmpeg -re -i "https://live.cnnindonesia.com/livecnn/smil:cnntv.smil/playlist.m3u8" -c:v copy -c:a aac -ar 44100 -ab 128k -ac 2 -strict -2 -flags +global_header -bsf:a aac_adtstoasc -bufsize 3000k -f flv "rtmp://localhost:8888/rtmp/' > cnn.sh
-cd
-cd shit/tv/
+echo 'ffmpeg -re -i "https://live.cnnindonesia.com/livecnn/smil:cnntv.smil/playlist.m3u8" -c:v copy -c:a aac -ar 44100 -ab 128k -ac 2 -strict -2 -flags +global_header -bsf:a aac_adtstoasc -bufsize 3000k -f flv "rtmp://localhost:8888/rtmp/"' > cnn.sh
 nano cnn.sh
 sh cnn.sh
 elif [ $jawab -eq 5 ];
 then
 cd tv
-echo 'youtube-dl -f best "https://youtu.be/XdZ2nJvmfrI" -o - | ffmpeg -re -i - -ar 44100 -vcodec copy -f flv "rtmp://localhost:8888/rtmp/' > tvone.sh
-cd
-cd shit/tv/
+echo 'youtube-dl -f best "https://youtu.be/XdZ2nJvmfrI" -o - | ffmpeg -re -i - -ar 44100 -vcodec copy -f flv "rtmp://localhost:8888/rtmp/"' > tvone.sh
 nano tvone.sh
 sh tvone.sh
 else
